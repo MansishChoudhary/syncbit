df12641568f64319a2cc07276177d8fd9872192e
MansishChoudhary <choudhary22081996@gmail.com>
2026-09-04 23:18:15 +0530

    Initial project files

---
+++ src/main/java/com/binarybrains/syncbit/SyncbitApplication.java
@@ -0,0 +1,13 @@
+package com.binarybrains.syncbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binarybrains/syncbit/SyncbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarybrains.syncbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
